f0ee71d95d0afc14e0ec473483f41d860ba2366c
diegovc2 <diegoveloso34@hotmail.com>
2026-04-16 22:12:15 +0200

    installation

---
+++ filament/filament-laravel/app/Filament/Resources/AutoGens/Pages/CreateAutoGen.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\AutoGens\Pages;
+
+use App\Filament\Resources\AutoGens\AutoGenResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateAutoGen extends CreateRecord
+{
+    protected static string $resource = AutoGenResource::class;
+}
